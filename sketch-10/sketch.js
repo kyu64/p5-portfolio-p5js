03745319e7b7422c8cf942778d92c7ae2299218d
kyu64 <serravezza.noe@gmail.com>
2026-05-02 10:05:59 +0200

    Update sketch.js

---
+++ sketch-10/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  
+  let x =0;
+  
+for( let x = 0; x <= 400; x+= 50){
+  fill (x / 2);
+  circle (x, height/2, 20);
+}
+    
+  
+    
+}
+
+
+function draw() {
+  
+}
